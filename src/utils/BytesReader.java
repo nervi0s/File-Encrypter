7daf75d73096f006a76b78e89e868953f74ac54b
nervi0s <nervi0s@protonmail.com>
2021-03-30 23:07:54 +0200

    Añadida clase para lectura de bytes

---
+++ src/utils/BytesReader.java
@@ -0,0 +1,47 @@
+package utils;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.RandomAccessFile;
+import java.util.concurrent.Callable;
+
+public class BytesReader implements Callable<byte[]> {
+
+    private final int numberBytesToRead; // Bytes a leer
+    private final byte[] chunkData; // Cantidad de datos a lamacenar
+    private final File file; // Archivo target
+    private final Integer bytesToSkip; // Puede que tenga que saltar bytes para leer la siguiente parte del archivo
+
+    public BytesReader(int bytesAmount, File f) { // Constructor si no hay que saltar bytes
+        numberBytesToRead = bytesAmount;
+        chunkData = new byte[numberBytesToRead];
+        file = f;
+        bytesToSkip = null;
+    }
+
+    public BytesReader(int bytesAmount, File f, Integer bytesToSkip) { // Cosntructor para saltar bytes
+        numberBytesToRead = bytesAmount;
+        chunkData = new byte[numberBytesToRead];
+        file = f;
+        this.bytesToSkip = bytesToSkip;
+    }
+
+    @Override
+    public byte[] call() throws Exception {
+        System.out.printf("Hilo %s ha empezado a leer %d bytes del fichero.%n", Thread.currentThread().getName(), numberBytesToRead);
+        readBytesFromFile(file);
+        return chunkData;
+    }
+
+    private void readBytesFromFile(File file) throws FileNotFoundException, IOException {
+        RandomAccessFile raf = new RandomAccessFile(file, "r");
+
+        if (bytesToSkip != null) {
+            raf.seek(bytesToSkip); // Saltamos bytes
+        }
+        int sizeDataReaded = raf.read(chunkData); // Almacenamos en chunkData la cantidad de chunkData.lenght bytes
+        System.out.printf("El hilo %s ha leído %d bytes.%n", Thread.currentThread().getName(), sizeDataReaded);
+    }
+
+}
--- src/utils/Encryptor.java
+++ src/utils/Encryptor.java
@@ -1,17 +1,25 @@
 package utils;
 
 import java.io.File;
+import java.io.FileInputStream;
 import java.io.FileNotFoundException;
-import java.io.RandomAccessFile;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 
 public class Encryptor {
 
     private final File file;
-    private final RandomAccessFile rFile;
+    private final FileInputStream fis;
 
     public Encryptor(File file) throws FileNotFoundException {
         this.file = file;
-        this.rFile = new RandomAccessFile(file, "rw");
+        this.fis = new FileInputStream(this.file);
+    }
+
+    private void delegateTasks() {
+        //Delegaremos a dos hilos que obtengan los bytes en crudo de un archivo
+        //El primer hilo obtendrá la mitad y el segundo el resto
+        ExecutorService executorService = Executors.newCachedThreadPool();
     }
 
 }
