ca2e3237219481b5c8a437dabc0477495d1e36ca
nervi0s <nervi0s@protonmail.com>
2021-04-01 00:14:35 +0200

    Creando el método para delegar taréas

--- src/utils/Encryptor.java
+++ src/utils/Encryptor.java
@@ -20,6 +20,18 @@ public class Encryptor {
         //Delegaremos a dos hilos que obtengan los bytes en crudo de un archivo
         //El primer hilo obtendrá la mitad y el segundo el resto
         ExecutorService executorService = Executors.newCachedThreadPool();
+
+        //Obtenemos la longitud en bytes del archivo
+        long totalBytesSize = file.length();
+        System.out.printf("El hilo delegador %s obtenido un archivo de %d .%n", Thread.currentThread().getName(), totalBytesSize);
+        //Obtenemos los bytes que leerá el primer hilo
+        long bytesTaksOne = totalBytesSize / 2;
+        //Obtenemos los bytes que leerá el segndo hilo
+        long bytesTaskTwo = totalBytesSize - bytesTaksOne;
+
+        //Creamos las tareas
+        BytesReader readerOne = new BytesReader((int) bytesTaksOne, file);
+        BytesReader readerTwo = new BytesReader((int) bytesTaskTwo, file, (int) bytesTaksOne);
     }
 
 }
