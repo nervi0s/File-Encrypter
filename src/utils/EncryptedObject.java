5e8b18f9ba4ad992ed59727259dfa3a890796923
nervi0s <nervi0s@protonmail.com>
2021-04-05 16:25:44 +0200

    Añadida función para unir las partes de datos en una sola parte

--- src/utils/EncryptedObject.java
+++ src/utils/EncryptedObject.java
@@ -1,5 +1,8 @@
 package utils;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class EncryptedObject {
 
     private final byte[] dataOne;
@@ -16,6 +19,25 @@ public class EncryptedObject {
 
     public byte[] getDataTwo() {
         return dataTwo;
+    }
+
+    public byte[] getAllData() {
+        List<Byte> bytesList = new ArrayList<>();
+
+        // Se añaden los primeros bytes
+        for (byte byte_ : getDataOne()) {
+            bytesList.add(byte_);
+        }
+        // Se añaden los últimos bytes
+        for (byte byte_ : getDataTwo()) {
+            bytesList.add(byte_);
+        }
+        // Se convierte la lista en array para su retorno
+        byte[] allbytes = new byte[bytesList.size()];
+        for (int i = 0; i < bytesList.size(); i++) {
+            allbytes[i] = bytesList.get(i);
+        }
+        return allbytes;
     }
 
 }
