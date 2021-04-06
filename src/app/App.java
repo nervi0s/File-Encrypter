401c069ac9c61707fa78c3c7c293eda5bea6100b
nervi0s <nervi0s@protonmail.com>
2021-04-07 00:20:00 +0200

    Borrados imports no usados

--- src/app/App.java
+++ src/app/App.java
@@ -1,15 +1,10 @@
 package app;
 
-import java.io.File;
-import java.io.FileNotFoundException;
-import java.util.logging.Level;
-import java.util.logging.Logger;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
-import utils.Encryptor;
 
 public class App extends Application {
 
